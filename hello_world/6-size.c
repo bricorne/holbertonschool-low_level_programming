ad1431397f94c69ccfdfea957f5a11d3845ac391
bricorne <bricepoitevineau@gmail.com>
2023-02-17 01:43:20 -0800

    6

---
+++ hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main() {
+int intType;
+long int longintType;
+long long int longlongintType;
+float floatType;
+char charType;
+
+// sizeof evaluates the size of a variable
+printf("Size of char: %zu bytes\n", sizeof(charType));
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of long int: %zu bytes\n", sizeof(longintType));
+printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
+printf("Size of float: %zu byte\n", sizeof(floatType));    
+return 0;
+}
