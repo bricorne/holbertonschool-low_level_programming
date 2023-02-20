cc9a34222f919dcfb0ed9279cf36f7439e609bbe
bricorne <bricepoitevineau@gmail.com>
2023-02-20 03:51:02 -0800

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+for (c = 'A' ; c <= 'Z' ; c++)
+{
+putchar(c);
+putchar(tolower(c));
+}
+printf("\n");
+return (0);
+}
