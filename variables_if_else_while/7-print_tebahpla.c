7fb7b181ce8aad7c22a5347a74dd0096105541f7
bricorne <bricepoitevineau@gmail.com>
2023-02-20 06:24:49 -0800

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+for (c = 'z' ; c => 'a' ; c--)
+{
+putchar(c);
+}
+printf("\n");
+return (0);
+}
