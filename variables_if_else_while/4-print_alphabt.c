4add79fb9e7cecbec557ffea36446a19e59ffde5
bricorne <bricepoitevineau@gmail.com>
2023-02-20 04:28:21 -0800

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+for (c = 'A' ; c <= 'Z' ; c++)
+{
+if (c != q && c != e)
+{
+putchar(tolower(c));
+}
+}
+printf("\n");
+return (0);
+}
