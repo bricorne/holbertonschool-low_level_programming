38c7eba68f664a2ca81ea746c41a12b37239ba85
bricorne <bricepoitevineau@gmail.com>
2023-02-21 02:12:37 -0800

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,8 +8,13 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+void print_alphabet(void)
 {
-  print_alphabet(void);
-  return (0);
+  char a;
+
+  for (a = 'a', a <= 'z'; a++)
+    {
+     _putchar(a);
+    }
+  _putchar("\n");;
 }
