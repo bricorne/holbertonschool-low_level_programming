a3d2c4a016158229490afd367bc9876c5fbadc72
bricorne <bricepoitevineau@gmail.com>
2023-02-21 04:25:00 -0800

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * int_islower - return if its lower or no
+ */
+
+int_islower(int c)
+{
+if ( islower(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
