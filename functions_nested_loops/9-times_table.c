4aa2e3cb36398404652349b18a30db7a36a3ce4d
bricorne <bricepoitevineau@gmail.com>
2023-02-22 06:27:12 -0800

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,13 +11,13 @@ void times_table(void)
 {
 int num=0, res, i, j;
 
-for(i=0; i<10; i++)
+for(i = 0; i < 10; i++)
     {
-      printf("%d ", num);
-      for(j=1; j<=10; j++)
+      printf("0");
+      for(j = 1; j < 10; j++)
 	{
 	  res = num*j;
-	  printf("%d  ", res);
+	  printf("%d,  ", res);
 	}
       printf("\n");
       num++;
