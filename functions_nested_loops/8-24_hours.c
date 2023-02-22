113f4414043a809450873790f34b3fef53523077
bricorne <bricepoitevineau@gmail.com>
2023-02-22 01:42:44 -0800

    8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_last_digit - computes the absolute value of an integer
+ * @r: An input character
+ * Return: always last digit
+ */
+
+void jack_bauer(void)
+{
+  int i:
+    for (i = 0; i < 24; i++)
+      {
+	int j;
+	for (j = 0; j < 60; j++)
+	  {
+	    printf("%d:%d\n",i,j);
+	  }
+      }
+  return (0);
+}
