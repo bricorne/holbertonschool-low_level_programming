17a1d80b591d3d87ec9a0d92d1c8f464303181a2
bricorne <bricepoitevineau@gmail.com>
2023-02-22 07:41:50 -0800

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -17,14 +17,28 @@ void print_to_98(int n)
     {
       for (i = n; i < 99; i++)
 	{
-	  _putchar(i);
+	  if (i != 98)
+	    {
+	  printf("%d, ", i);
+	    }
+	  else
+	    {
+	      printf("%d", i);
+	    }
 	}
     }
   else if (n > 98)
     {
       for (i = n; i > 98; i--)
 	{
-	  _putchar(i);
+	  if (i != 98)
+	    {
+	      printf("%d, ", i);
+	    }
+	  else
+	    {
+	      printf("%d", i);
+	    }
 	}
     }
 else
