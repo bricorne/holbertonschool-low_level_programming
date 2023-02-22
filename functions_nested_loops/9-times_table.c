f47739e3ffb7564d7d583127bec15f63f1197061
bricorne <bricepoitevineau@gmail.com>
2023-02-22 02:32:13 -0800

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * time_table - print hours
+ * Return: always last digit
+ */
+
+void times_table(void)
+{
+int num=0, res, i, j;
+
+for(i=0; i<10; i++)
+    {
+      printf("%d ", num);
+      for(j=1; j<=10; j++)
+	{
+	  res = num*j;
+	  printf("%d  ", res);
+	}
+      printf("\n");
+      num++;
+    }
+  getch();
+  return 0;
+}
