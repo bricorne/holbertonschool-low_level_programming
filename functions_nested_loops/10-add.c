f389f6c939f5377530caec16f8a7bca7c232fefb
bricorne <bricepoitevineau@gmail.com>
2023-02-22 07:07:14 -0800

    10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -4,11 +4,13 @@
 #include "main.h"
 /**
  * add - print hours
+ *@a: value 1
+ *@b: value 2
  * Return: always last digit
  */
 
 int add(int a, int b)
 {
-  int n = a + b;
-  return (n);
+int n = a + b;
+return (n);
 }
