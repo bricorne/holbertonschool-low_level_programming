bcae85faf3c36d919d912b17920b00388239074a
bricorne <bricepoitevineau@gmail.com>
2023-02-23 05:33:28 -0800

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -17,13 +17,12 @@ int j;
 
 for (j = 0; j < n; j++)
 {
-  for (i = 0; i < j; i++)
-    {
+
+for (i = 0; i < j; i++)
+{
 _putchar(' ');
-    }
-  _putchar('\\');
-  _putchar('\n');
- }
+}
+_putchar('\\');
 _putchar('\n');
- 
+} 
 }
