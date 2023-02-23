e9eecf98f35a3ee2db523fc9e720594a86e3a09e
bricorne <bricepoitevineau@gmail.com>
2023-02-23 06:45:40 -0800

    9

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_triangle - Check Holberton
+ *@size: an input number
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+void print_triangle(int size)
+{
+int i;
+int j;
+
+for (j = 0; j < size; j++)
+{
+
+for (i = 0; i < size; i++)
+{
+_putchar(' ');
+}
+_putchar('#');
+_putchar('\n');
+}
+if (size <= 0)
+{
+putchar('\n');
+}
+}
