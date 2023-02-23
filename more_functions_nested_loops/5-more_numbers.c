507fe6cf3ba0cee6e13825baa2d1c0c14a1df2d1
bricorne <bricepoitevineau@gmail.com>
2023-02-23 04:52:43 -0800

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,16 +12,18 @@
 void more_numbers(void)
 {
 int j;
-int i;
-for (j = 0; j < 10; j++)
-{
 
-for (i = 0; i <= 14; i++)
-{
-  char n=(char)i;
-_putchar(n / 10);
-_putchar(n % 10);
-}
-_putchar('\n');
-}
+int i;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
