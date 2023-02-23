eaaa66589e8b834d0db86dc37420b95a86961f44
bricorne <bricepoitevineau@gmail.com>
2023-02-23 04:40:13 -0800

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * more_numbers - Check Holberton
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+void more_numbers(void)
+{
+int j;
+char i;
+for (j = 0; j < 10; j++)
+{
+
+for (i = '0'; i <= "14"; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
