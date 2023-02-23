dbb947e24e1b65094ebbe1e26b833b9d365b6f6d
bricorne <bricepoitevineau@gmail.com>
2023-02-23 05:27:17 -0800

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_line - Check Holberton
+ *@n: an input number
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+void print_diagonal(int n)
+{
+int i;
+int j;
+
+for (j = 0; j < n; j++)
+{
+  for (i = o; i < n; i++)
+    {
+_putchar(' ');
+    }
+  _putchar('\\');
+  _putchar('\n');
+ }
+_putchar('\n');
+ 
+}
