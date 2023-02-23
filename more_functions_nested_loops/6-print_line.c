58c51afd3689741aafe0fd5b907e4e36bc9227e6
bricorne <bricepoitevineau@gmail.com>
2023-02-23 05:07:25 -0800

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * more_numbers - Check Holberton
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+void print_line(int n)
+{
+  int i;
+  for (i = 0; i < n; i++);
+  {
+    _putchar('_');
+  }
+  _putchar('\n');
+}
