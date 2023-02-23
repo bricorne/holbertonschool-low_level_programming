c0368f2aa684cea80da09f488a1e650cc1f73cf2
bricorne <bricepoitevineau@gmail.com>
2023-02-23 04:25:44 -0800

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,9 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include "main.h"
 /**
- * print_numbers - Check Holberton
+ * print_most_numbers - Check Holberton
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: 1 if is lowercase or 0 if is uppercase
