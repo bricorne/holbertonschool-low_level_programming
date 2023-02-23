74325e61995c2a36955307967cf9cb7f9d55eb0e
bricorne <bricepoitevineau@gmail.com>
2023-02-23 06:05:43 -0800

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_diagonal - Check Holberton
+ *@n: an input number
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+void FizzBuzz(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 5 == 0)
+{
+printf("Fizz ");
+}
+if (i % 3 == 0)
+{
+printf("Buzz ");
+}
+}
+}
