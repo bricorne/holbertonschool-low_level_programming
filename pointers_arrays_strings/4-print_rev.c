0c74619115efdd5ca19e7b4884775d96043ae452
bricorne <bricepoitevineau@gmail.com>
2023-02-27 04:41:55 -0800

    4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,5 +10,26 @@
 
 void print_rev(char *s)
 {
-printf("%s\n", strrev(s));
+{
+   int length, c;
+   
+   char *begin, *end, temp;
+ 
+   length = string_length(s);
+   begin  = s;
+   end    = s;
+ 
+   for (c = 0; c < length - 1; c++)
+      end++;
+ 
+   for (c = 0; c < length/2; c++)
+   {        
+      temp   = *end;
+      *end   = *begin;
+      *begin = temp;
+ 
+      begin++;
+      end--;
+   }
+}
 }
