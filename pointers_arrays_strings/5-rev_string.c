df231c771068eec1051a1892711fea0c3b994483
bricorne <bricepoitevineau@gmail.com>
2023-02-27 06:08:43 -0800

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * rev_string - Check Holberton
+ *@s: an input
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+void rev_string(char *s)
+{
+  int len = 0;
+  int i = 0;
+  char x;
+  
+  while (s[len] != '\0')
+    {
+      len++;
+    }
+  
+  while (i < len--)
+    {
+      x = s[i];
+      s[i++] = x[len];
+      s[len] = x;
+    }
+}
