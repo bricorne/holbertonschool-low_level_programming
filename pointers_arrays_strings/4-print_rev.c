914fccf81cd516d3e6ca27bc59bfd7b534c62cda
bricorne <bricepoitevineau@gmail.com>
2023-02-27 04:06:33 -0800

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - Check Holberton
+ *@s: an input
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+void print_rev(char *s)
+{
+  printf("%s\b", strrev(str));
+}
