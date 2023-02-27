e88c8e8a37dda27139c69efe2ee794381aa9b70b
bricorne <bricepoitevineau@gmail.com>
2023-02-27 01:45:18 -0800

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * reset_to_98 - Check Holberton
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+void reset_to_98(int *n)
+{
+  *n=98;
+}
