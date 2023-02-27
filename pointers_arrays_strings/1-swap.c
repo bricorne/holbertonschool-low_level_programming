aae284d05db6ff50ec988ebe61a2fa8ebcaa5f6b
bricorne <bricepoitevineau@gmail.com>
2023-02-27 01:53:34 -0800

    1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * swap_int - Check Holberton
+ *@a: an input
+ *@b: an input
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+void swap_int(int *a, int *b)
+{
+*a=*b;
+*b=*a;
+}
