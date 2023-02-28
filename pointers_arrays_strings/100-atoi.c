952958ce2a7ae30e2343ee66b4616926a5d5380c
bricorne <bricepoitevineau@gmail.com>
2023-02-28 05:04:03 -0800

    100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,23 +10,6 @@
 
 int _atoi(char *s)
 {
-int i;
-int x;
-int len = 0;
-while (s[len] != '\0')
-{
-len++;
-}
- for (i = 0; i < len; i++)
-   {
-     if (s[i] > '0' || s[i] < '9' || s[i] == '+' || s[i] == '-')
-       {
-	 x = s[i];
-       }
-     else
-       {
-	 x = 0;
-       }
-   }
+  int x = atoi(s);
  return (x);
 }
