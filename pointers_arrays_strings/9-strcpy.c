f900644813a816470e19d5379665d1b01ae45a66
bricorne <bricepoitevineau@gmail.com>
2023-02-28 04:13:36 -0800

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *_strcpy - Check Holberton
+ *@dest: an input
+ *@src: an input
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+  strcpy(dest, src);
+  return (dest);
+}
