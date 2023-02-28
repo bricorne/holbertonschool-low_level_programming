df929e23ecbd1e6318408ddd1eaafab67f61018a
bricorne <bricepoitevineau@gmail.com>
2023-02-28 01:41:18 -0800

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,19 +10,19 @@
 
 void rev_string(char *s)
 {
-  int len = 0;
-  int i;
-  char x = s[0];
-  
-  while (s[len] != '\0')
-    {
-      len++;
-    }
-  
-  for (i = 0; i < len / 2; i++)
-    {
-      x = s[i];
-      s[i] = s[len--];
-      s[len--] = x;
-    }
+int len = 0;
+int i;
+char x;
+
+while (s[len] != '\0')
+{
+len++;
+}
+
+while (i < len--)
+{
+x = s[i];
+s[i++] = s[len];
+s[len] = x;
+}
 }
