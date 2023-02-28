553d583852a77de22c47d8ad48a1db89418ff1c5
bricorne <bricepoitevineau@gmail.com>
2023-02-28 02:26:36 -0800

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - Check Holberton
+ *@str: an input
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+void puts_half(char *str)
+{
+int len = 0;
+int i;
+
+while (str[len] != '\0')
+{
+len++;
+}
+for (i = len / 2; i < len; i++)
+{
+printf("%c", str[i]);
+}
+printf("\n");
+}
