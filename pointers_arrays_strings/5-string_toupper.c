347b40c96e3849ce6ec5e5878e5b39acd24cfc9d
bricorne <bricepoitevineau@gmail.com>
2023-03-01 07:15:54 -0800

    5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *string_toupper - Check Holberton
+ *@*: an input
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+char *string_toupper(char *n)
+{
+    int i;
+    for (i = 0; n[i] != '\0'; i++)
+    {
+            if (n[i] >= 'a' && n[i] <= 'z')
+                    n[i] =(int)n[i] - 32;
+    }
+    return (n);
+ }
