5ce36bff375ccfc64091a23d9124fb9bff60382a
bricorne <bricepoitevineau@gmail.com>
2023-03-01 03:11:37 -0800

    1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *_strncat - Check Holberton
+ *@dest: an input
+ *@src: an input
+ *@n: an input
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+
+while (dest[i] != '\0')
+i++;
+while (src[j] != '\0')
+{
+
+while (j < n)
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+}
+dest[i] = '\0';
+return (dest);
+}
