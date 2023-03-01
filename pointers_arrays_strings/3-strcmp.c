f9b51e829cf4d0850f0c56ee30a3983dbd1c5743
bricorne <bricepoitevineau@gmail.com>
2023-03-01 05:57:37 -0800

    3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *_strncat - Check Holberton
+ *@s2: an input
+ *@s1: an input
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
+}
