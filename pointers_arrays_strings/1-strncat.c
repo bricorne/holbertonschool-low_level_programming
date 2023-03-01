07a1be47f814a640a53ff8953b2a7e51415c3810
bricorne <bricepoitevineau@gmail.com>
2023-03-01 04:36:34 -0800

    1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,6 +12,7 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+{
 int i = 0;
 int j = 0;
 
@@ -19,13 +20,10 @@ while (dest[i] != '\0')
 i++;
 while (src[j] != '\0')
 {
-
-while (j < n)
-{
 dest[i] = src[j];
 j++;
 i++;
 }
-}
+dest[i] = '\0';
 return (dest);
 }
