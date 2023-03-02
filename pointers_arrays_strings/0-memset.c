86a9fcc0e80351f1265a6bd7d79f4cddc0c84147
bricorne <bricepoitevineau@gmail.com>
2023-03-02 05:10:46 -0800

    0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *_memset - Check Holberton
+ *@s: an input
+ *@b: an input
+ *@n: an input
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+  _memset(s, b, n);
+  return (0);
+}
