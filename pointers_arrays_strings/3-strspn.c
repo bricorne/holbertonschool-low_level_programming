9ed7f3404e21db2440697c3ba13d5af1201ab797
bricorne <bricepoitevineau@gmail.com>
2023-03-05 08:58:58 -0800

    3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,6 +11,26 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-int b = strspn(s, accept);
-return (b);
+  int count = 0, flag;
+  char *start = accept;
+
+  while (*s)
+    {
+      flag = 0;
+      while (*accept)
+	{
+	  if (*accept == *s)
+	    {
+	      count++;
+	      flag = 1;
+	      break;
+	    }
+	  accept++;
+	}
+      s++;
+      accept = start;
+      if (flag == 0)
+	break;
+    }
+  return (count);
 }
