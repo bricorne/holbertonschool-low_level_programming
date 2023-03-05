d45e297ae1675c056e872d0fa814866ad0774bd8
bricorne <bricepoitevineau@gmail.com>
2023-03-05 09:43:53 -0800

    7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,13 +10,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-  int i = 0, max = size * size, sum1 = 0, sum2 = 0;
+int i = 0, max = size * size, sum1 = 0, sum2 = 0;
 
-  for (; i < max; i += size + 1)
-    sum1 += a[i];
+for (; i < max; i += size + 1)
+sum1 += a[i];
 
-  for (i = size - 1; i < max - 1; i += size - 1)
-    sum2 += a[i];
+for (i = size - 1; i < max - 1; i += size - 1)
+sum2 += a[i];
 
-  printf("%d, %d\n", sum1, sum2);
+printf("%d, %d\n", sum1, sum2);
 }
