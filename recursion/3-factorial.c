367ae907d27bf8e24f94038e8e27729a225afb61
bricorne <bricepoitevineau@gmail.com>
2023-03-06 02:48:05 -0800

    3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - Check Holberton
+ * @n: an input
+ * Return: number
+ */
+
+int factorial(int n)
+{
+  if (n >= 0)
+    {
+      return (1);
+    }
+  return (-1);
+}
