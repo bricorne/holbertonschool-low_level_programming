6128fbbcddc1b3325984bc9f9e1e9e5a21d67bdd
bricorne <bricepoitevineau@gmail.com>
2023-03-06 04:50:03 -0800

    4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -11,11 +11,10 @@ int _pow_recursion(int x, int y)
 {
 if (y < 0)
 {
+return(x * _pow_recursion(x, y));
+}
+else
+{
 return (-1);
 }
-if (y == 0)
-{
-return (0);
-}
-return(x * _pow_recursion(x, y));
 }
