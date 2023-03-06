59575d7e8df35294945611a5890969b15b556740
bricorne <bricepoitevineau@gmail.com>
2023-03-06 01:26:34 -0800

    1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Check Holberton
+ *@s: an input
+ */
+
+void _print_rev_recursion(char *s)
+{
+strrev(s);
+puts(s);
+}
