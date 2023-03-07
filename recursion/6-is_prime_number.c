a4286150d783302e230574bbebd45177b784752c
bricorne <bricepoitevineau@gmail.com>
2023-03-07 01:29:59 -0800

    6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,7 +1,36 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * is_prime_number - Check Holberton
- * @y: an input
- * Return: number
- */
+int primeno(int, int);
+
+int is_prime_number(int n)
+{
+ int check = primeno(n, n / 2);
+  if (check == 1)
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
+}
+
+int primeno(int n  , int i)
+{
+  if (i == 1)
+    {
+      return 1;
+    }
+  else
+    {
+      if (n % i == 0)
+	{
+	  return 0;
+	}
+      else
+	{
+	  return primeno(n, i - 1);
+	}
+    }
+}
