5fde9a75b69790fde0d8e9afead516983c57118a
bricorne <bricepoitevineau@gmail.com>
2023-03-07 01:48:44 -0800

    6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,47 +1,38 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * is_prime_number - It returns if hes prime
- * @n: an input integer
- * @i: an input
- * Return: 1 or 0
- */
+int check_prime(int n, int i);
 
-int primeno(int n, int i);
+/**
+ * is_prime_number - Returns if a number is prime
+ * @n: the number to be checked
+ *
+ * Return: 0
+ */
 
 int is_prime_number(int n)
 {
-int check = primeno(n, n / 2);
-if (check == 1)
-{
-return (1);
-}
-else
-{
-return (0);
-}
+  return (check_prime(n, 1));
 }
 
 /**
- * primeno - It returns the value of square root of n.
- * @n: an input integer
- * @i: an input integer
- * Return: The square root of n
+ * check_prime - Check if number is prime
+ * @n: the number to be checked
+ * @i: the iteration
+ *
+ * Return: 0
  */
 
-int primeno(int n, int i)
+int check_prime(int n, int i)
 {
-if (i == 1)
-{
-return (1);
-}
-else if (n % i == 0)
-{
-return (0);
-}
-else
-{
-return (primeno(n, i - 1));
-}
+  if (n <= 1)
+    return (0);
+
+  if (n % i == 0 && i > 1)
+    return (0);
+
+  if ((n / i) < i)
+    return (1);
+
+  return (check_prime(n, i + 1));
 }
