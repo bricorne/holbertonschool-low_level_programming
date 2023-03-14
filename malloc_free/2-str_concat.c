552686777e4da1249d9d5434418279e24f072ec2
bricorne <bricepoitevineau@gmail.com>
2023-03-14 08:25:35 -0700

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,21 +12,41 @@
 
 char *str_concat(char *s1, char *s2)
 {
-char *s;
-int i = strlen(s1);
-int y = strlen(s2);
-int r = y + i;
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
 
-s = malloc((sizeof(char) * r) + 1);
+	if (s1 == NULL)
+		s1 = "";
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-strcat(s, s1);
-strcat(s, s2);
+	while (s1[i])
+		i++;
 
-s[r] = '\0';
-return (s);
+	while (s2[j])
+		j++;
+
+	l = i + j;
+	s = malloc((sizeof(char) * l) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	j = 0;
+
+	while (k < l)
+	{
+		if (k <= i)
+			s[k] = s1[k];
+
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+		k++;
+	}
+	s[k] = '\0';
+	return (s);
 }
