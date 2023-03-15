e161d96690e3d23cee73ac758a1a5913fb93a308
bricorne <bricepoitevineau@gmail.com>
2023-03-15 05:45:49 -0700

    0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,20 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * malloc_checked - function returns a pointer to a newly allocated space in memory
+ * @b: the size
+ *
+ * Return: 0
+ */
+
 void *malloc_checked(unsigned int b)
 {
-  char *s;
-  s = malloc(b);
-  return (s);
+char *s;
+s = malloc(b);
+if (s == NULL)
+{
+return ("98");
+}
+return (s);
 }
