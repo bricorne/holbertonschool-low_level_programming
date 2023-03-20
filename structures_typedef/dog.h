f58a4d6ba65debcd98b0db0be97e842700596e85
bricorne <bricepoitevineau@gmail.com>
2023-03-20 03:04:56 -0700

    0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog attributes
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char * owner;
 };
+#endif
