c79790cff8e1c283058423425cb04969953094de
bricorne <bricepoitevineau@gmail.com>
2023-03-20 06:51:24 -0700

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -21,26 +21,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	while (owner[len_owner])
 		len_owner++;
 	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	
+	if (new_dog == NULL || name == NULL || owner == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
+	
 	cpyname = malloc(len_name + 1);
-	if (cpyname == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
+
 	for (i = 0; name[i]; i++)
 		cpyname[i] = name[i];
 	cpyname[i] = '\0';
+
 	cpyowner = malloc(len_owner + 1);
-	if (cpyowner == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
+
 	for (i = 0; owner[i]; i++)
 		cpyowner[i] = owner[i];
 	cpyowner[i] = '\0';
