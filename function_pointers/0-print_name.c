0c991cd9610df808331e157998f710b0e9d1d7fa
bricorne <bricepoitevineau@gmail.com>
2023-03-21 04:11:13 -0700

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,15 +2,15 @@
 #include "function_pointers.h"
 
 /**
-  * print_name - creates an array of chars
+  * print_name - print name
   * @name: name to print
-  *
+  * @f: fonction
   * Return: 0
   */
 
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL)
+if (name != NULL && f != NULL)
 {
 	f(name);
 }
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,3 +1,8 @@
+#ifndef HEADER
+#define HEADER
+
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
+
+#endif
 
