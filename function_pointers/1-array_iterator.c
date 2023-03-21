4f0b7481b19235e1b7b77a5e7eced2d9c7526fb6
bricorne <bricepoitevineau@gmail.com>
2023-03-21 05:34:37 -0700

    1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -14,11 +14,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 unsigned int i = 0;
 
 if (array != NULL && size > 0 && action != NULL)
+{
+	while (i < size)
 	{
-		while (i < size)
-		{
-			action(array[i]);
-			i++;
-		}	
+		action(array[i]);
+		i++;
 	}
+}
 }
