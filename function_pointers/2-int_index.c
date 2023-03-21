6635c294bead7aa2922a387bb5cccdd4d2048e7e
bricorne <bricepoitevineau@gmail.com>
2023-03-21 06:03:44 -0700

    2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+  * int_index - search
+  * @array: array
+  * @size: the size
+  * @cmp: fonction
+  * Return: 0
+  */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i = 0;
+
+if (size <= 0)
+	return (-1);
+if (array != NULL && cmp != NULL)
+{
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (cmp(array[i]));
+		}
+		i++;
+	}
+}
+return (-1);
+}
