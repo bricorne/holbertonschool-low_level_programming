dd832a4d506c121882708e4807badfbd7cc964f4
bricorne <bricepoitevineau@gmail.com>
2023-03-21 05:28:05 -0700

    1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+  * array_iterator - print name
+  * @array: array
+  * @size: the size
+  * @f: fonction
+  * Return: 0
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i = 0;
+
+if (array != NULL && size > 0 && action != NULL)
+	{
+		while (i <= size)
+		{
+			action(array[i]);
+			i++;
+		}	
+
+	}
+}
