0a6bdf3c3078382fadbb265d8faf0f92811aba39
bricorne <bricepoitevineau@gmail.com>
2023-03-22 08:54:32 -0700

    3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * op_add - +
+ * op_sug - -
+ * op_mul - *
+ * op_div - /
+ * op_mod - %
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return(a * b);
+}
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return(a / b);
+}
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
