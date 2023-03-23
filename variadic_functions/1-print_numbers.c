7cbd6bea9884ea5c5cb39e45e2222df19e9eed78
bricorne <bricepoitevineau@gmail.com>
2023-03-23 04:55:16 -0700

    1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,13 +15,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
-	if(separator == NULL)
-		separator = "";
-	
 	va_start(list, n);
 	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL || i == (n - 1))
+			separator = "";
 		printf("%d%s", va_arg(list, int), separator);
-	
+	}
+
 	va_end(list);
 	printf("\n");
 }
