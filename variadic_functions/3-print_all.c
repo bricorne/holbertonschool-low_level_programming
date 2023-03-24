864c7ae23baa1ee916099ba99dc306f5b528cc6d
bricorne <bricepoitevineau@gmail.com>
2023-03-24 06:54:19 -0700

    betty

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 {
 	va_list ap;
 	char *temp;
-	int i = 0;
+	int i;
 
 	va_start(ap, format);
 	while (format == NULL)
@@ -21,7 +21,7 @@ void print_all(const char * const format, ...)
 		printf("\n");
 		return;
 	}
-	while (format[i])
+	for (i = 0; format[i]; i++)
 	{
 		switch (format[i])
 		{
@@ -44,10 +44,9 @@ void print_all(const char * const format, ...)
 				printf("(nil)");
 				break;
 		}
-		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') 
-				&& format[(i + 1)] != '\0')
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
+		|| format[i] == 's') && format[(i + 1)] != '\0')
 			printf(", ");
-		i++;
 	}
 	va_end(ap);
 	printf("\n");
