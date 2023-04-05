c9179fe642c43377260174019efa654a0f34209b
bricorne <bricepoitevineau@gmail.com>
2023-04-05 06:20:34 -0700

    task 4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,13 +1,16 @@
 #include "lists.h"
 
 /**
-  *
-  *
-  *
-  *
+  * free_list - free the list
+  * @
   */
 
 void free_list(list_t *head)
 {
-	free(head);
+	while (head)
+	{
+		free (head->str);
+		head = head->next;
+	}
+	free (head);
 }
