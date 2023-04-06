f8e8f8697c1394e15c23d9caf79a41bf8f2e5ad3
bricorne <bricepoitevineau@gmail.com>
2023-04-06 03:22:05 -0700

    task2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -16,6 +16,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	a->n = n;
 	a->next = *head;
 	a->prev = NULL;
+
+	if(*head != NULL)
+	{
+		(*head)->prev = a;
+	}
+
 	*head = a;
 	return (a);
 }
