7da204e1ccb383c66476f61f7ea7b90f72bae967
bricorne <bricepoitevineau@gmail.com>
2023-04-06 02:56:12 -0700

    task2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  * add_dnodeint - A function that adds a node to the head of linked list
+  * @head: pointer to a list_t pointer that points to the head struct
+  * @n: pointer to int that adds as node
+  * Return: pointer to new head of list, NULL on failure
+  */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *a;
+
+	a = malloc(sizeof(dlistint_t));
+
+	a->n = n;
+	a->next = *head;
+	*head = a;
+	return (a);
+}
