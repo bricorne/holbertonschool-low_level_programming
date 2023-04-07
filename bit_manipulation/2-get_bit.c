71e86a46520ef8b3f6103c28c0e5e3526018e470
bricorne <bricepoitevineau@gmail.com>
2023-04-07 07:38:32 -0700

    task 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -10,5 +10,8 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int result = n >> index;
+
+	if (index >= sizeof(unsigned long int) * CHAR_BIT)
+        	return -1;
 	return (result & 1);
 }
