b7a8f4918b63681fbae4527e93da7439b68bc028
bricorne <bricepoitevineau@gmail.com>
2023-04-11 06:15:35 -0700

    task 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_create - function that create hash table
+  * @size: the size of the array
+  * Return: the hash table
+  */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+
+	return (hash_table);
+}
