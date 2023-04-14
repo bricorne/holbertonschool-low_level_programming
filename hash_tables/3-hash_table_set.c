9aaa5e2f882642d203c308fcb28ca9efeb981cb8
bricorne <bricepoitevineau@gmail.com>
2023-04-14 06:12:16 -0700

    task3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,23 +1,52 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_set -adds an element to the hash table
+ * @key: the key
+ * @value: value inside the node
+ * @ht: ptr
+ * Return: the element
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
-	unsigned long int hash_value;
-	int bucket_index;
+	hash_node_t *new;
+	char *copy_value;
+	unsigned long int index, i;
 
-	if (!ht || !key || !value || key[0] == '\0')
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	hash_value = hash_djb2(key);
-	bucket_index = hash_value % ht->size;
 
-	 new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
-	 if (new_node == NULL)
-		 return 0;
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = NULL;
-	
-	ht->array[bucket_index] = new_node;
+	copy_value = strdup(value);
+	if (copy_value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = copy_value;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+
+	new->value = copy_value;
+	new->next  = ht->array[index];
+
+	ht->array[index] = new;
+
 	return (1);
 }
